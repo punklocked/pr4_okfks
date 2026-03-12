dcd52f5b56d15d6e1b3c0afc48ec33794d5135cc
punklocked <olegkurlykovic@gmail.com>
2026-03-12 19:03:12 +0500

    added geometry classes

---
+++ GeometryShape/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+            try
+            {
+                if (height <= 0 || width <= 0)
+                    throw new ArgumentException("Некорректные значения сторон");
+                if (height == width)
+                    throw new ArgumentException("У прямоугольника не может быть одинаковая высота и ширина");
+            }
+            catch
+            {
+                throw new ArgumentException("Некорректные входные данные");
+            }
+
+        }
+
+        private double width { get; set; }
+        private double height { get; set; }
+
+        public override double Area()
+        {
+            return width * height;
+        }
+
+        public override double Perimeter()
+        {
+            return (height * 2) + (width * 2);
+        }
+
+        public override string ToString()
+        {
+            return $"Ширина прямоугольника - {width}\nВысота прямоугольника - {height}";
+        }
+    }
+}
---
+++ GeometryShape/Triangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle : Shape
+    {
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+            try
+            {
+                if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                    throw new ArgumentException("Стороны треугольника не должны быть меньше или равны нулю");
+                if (sideA + sideB < sideC
+                    || sideA + sideC < sideB
+                    || sideB + sideC < sideA)
+                    throw new ArgumentException("Некорректные данные для треугольника");
+            }
+            catch
+            {
+                throw new ArgumentException("Некорректные входные данные");
+            }
+        }
+
+        private double sideA { get; set; }
+        private double sideB { get; set; }
+        private double sideC { get; set; }
+
+        public override double Area()
+        {
+            double p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+
+        public override double Perimeter()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        public override string ToString()
+        {
+            return $"Сторона A: {sideA}\nСторона B: {sideB}\nСторона C: {sideC}";
+        }
+    }
+}
